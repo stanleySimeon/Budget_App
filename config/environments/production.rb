5f18945daf13aba68c5f31db11d274ba438ec621
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-17 00:52:55 +0100

    vendor compiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.cache_classes = true
   image_tag '#', skip_pipeline: true
 
-
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.css *.js *.eot *.woff *.ttf *.svg)
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
