c80b8b470833c09f7bfd74a4bd4f50915186c086
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-17 04:57:52 +0100

    fix rubocop error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     return unless user.logger?
-    
+
     can :manage, :all
     can :read, :all
     can :manage, Payment, user_id: user.id
