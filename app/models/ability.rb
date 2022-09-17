aacb4f9a4b61b99b4119e74e0256714b36faa9aa
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-17 18:27:37 +0100

    Update ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    return unless user.logger?
+    can :read, Category
+    can :create, Category
+    can :write, Category
 
-    can :manage, :all
-    can :read, :all
-    can :manage, Payment, user_id: user.id
-    can :manage, Category, user_id: user.id
+    can :read, Payment
+    can :create, Payment
+
+    return unless user.present?
+
+    can :destroy, Category, user: user
+    can :destroy, Payment, user: user
   end
 end
