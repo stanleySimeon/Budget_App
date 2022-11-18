71e7e6738a5457bfa71c203e6c4de0aa2b544898
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-11-18 09:03:59 -0500

    Add features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :update_allowed_parameters, if: :devise_controller?
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
+  end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :image)
+    end
   end
 end
