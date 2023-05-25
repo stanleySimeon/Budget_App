1b3ecaa3cfb4cef14a24df6ef815dd5c9d3728bf
Stanley SIMEON <mstanley.me@gmail.com>
2023-05-24 23:29:02 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,6 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  # image_tag '#', skip_pipeline: true
-
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
