5d4175db8d7228bd0684335f2db25ee22cb55692
Stanley SIMEON <mstanley.me@gmail.com>
2023-05-26 15:32:03 -0400

    update SplashesController

--- app/controllers/splashes_controller.rb
+++ app/controllers/splashes_controller.rb
@@ -1,5 +1,6 @@
 class SplashesController < ApplicationController
   def index
-    redirect_to categories_path if user_signed_in?
-  end
+    # redirect_to categories_path if user_signed_in?
+  end;
 end
+
