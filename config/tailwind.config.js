f893479ece68a57cb698befdcca2b0dd4ed7980c
Stanley SIMEON <mstanley.me@gmail.com>
2023-06-05 18:38:02 -0400

    Update Design

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -25,6 +25,18 @@ module.exports = {
           borderLight: '#FAF7F7',
         },
       },
+      keyframes: {
+        bounce_to_right: {
+          '0%, 100%': {
+            transform: 'translateX(0)',
+            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateX(10px)',
+            animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        }
+      },
     },
   },
   plugins: [],
